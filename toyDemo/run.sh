6a2e2419ab144cd82a528894c23d2fa74b90a3b4
DongyuXu77 <3079472743@qq.com>
2022-01-09 19:52:56 +0800

    delete a unused dir

--- toyDemo/run.sh
+++ toyDemo/run.sh
@@ -1,5 +1,11 @@
-#/bin/bash
+# /usr/bin
 
-git diff test.docx
-
-sleep 30
+# fileName=$( git ls-files --other --exclude-standard ).log
+# touch "$fileName"
+git add $(git ls-files -o --exclude-standard)
+git diff --quiet; noChanges=$?
+if [ $noChanges -eq 1 ]; then
+    git diff test.docx >>./diff.log
+    echo -e "\n\n\n">>./diff.log
+    git add test.docx
+fi
