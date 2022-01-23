fb361ec8e085b2edb7769114f263ab0c33dde2f7
DongyuXU77 <Xudongyu@Bytedance.com>
2022-01-23 23:20:16 +0800

    change some parameters and configuration

--- utils/capture.py
+++ utils/capture.py
@@ -1,10 +1,14 @@
-import pyscreenshot
 import time
+from PIL import Image
+import pyscreenshot
 
-def screenshot(path='./test.png'):
-	time.sleep(10)
+def screenshot(path='./test.png', ratio=2.0):
+	time.sleep(5)
 	image = pyscreenshot.grab()
+	height, width = image.size
+	print(image.size)
+	image = image.resize((int(height/ratio), int( width/ratio)), Image.ANTIALIAS)
 	image.save(path)
-
+	print(image.size)
 if __name__=="__main__":
 	screenshot()
--- utils/segment.py
+++ utils/segment.py
@@ -1,34 +1,27 @@
-from PIL import Image
-import cv2
+import os
 import numpy as np
+from PIL import Image
 from matplotlib import pyplot
-kernel = 0.06*np.ones((5,5))
-def test():
-	image = cv2.imread('./test.png')
-	pyplot.imshow(image)
-	#pyplot.show()
-	img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	mask_kernel = cv2.filter2D(image, -1, kernel)
-	hsv = cv2.cvtColor(mask_kernel, cv2.COLOR_BGR2Lab)
-	low_hsv = np.array([180, 0, 0], dtype=np.uint8)
-	high_hsv = np.array([225, 225, 225], dtype=np.uint8)
 
-	mask = cv2.inRange(hsv, low_hsv, high_hsv)
-	opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-	ret, threshl = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-
-	pyplot.imshow(image[:, :, 1], "gray")
-	pyplot.show()
-def test_1():
-	image = Image.open('./test.png')
-	image_1 = image.convert('L')
-	image_1 = np.array(image_1)
-	
-	Threshold = 254
-	for l_1 in range(len(image_1)):
-		for l_2 in range(len(image_1[0])):
-			image_1[l_1][l_2] = 255 if image_1[l_1][l_2]>Threshold else 0
-	pyplot.imshow(image_1)
-	pyplot.show()
+def grayImage(path):
+	image = Image.open(path)
+	image_gray = image.convert('L')
+	image_gray = np.array(image_gray)
+	Threshold = 255
+	for l_1 in range(len(image_gray)):
+		count = 0
+		for l_2 in range(len(image_gray[1])):
+			if image_gray[l_1][l_2]>=Threshold:
+				image_gray[l_1][l_2] = 255
+				count = count+1
+			else:
+				image_gray[l_1][l_2] = 0
+		if count/len(image_gray[1])<0.3:
+			for l_2 in range(len(image_gray[1])):
+				image_gray[l_1][l_2] = 0
+	if (os.path.exists('./saved_picture')) is False:
+		os.makedirs('./saved_picture')
+	image_gray = Image.fromarray(image_gray)
+	image_gray.save('./saved_picture/'+os.path.basename(path))
 if __name__=="__main__":
-	test_1()
+	grayImage('./test.png')
